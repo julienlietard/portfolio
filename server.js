f5f2bd3144f573bdd8a048525bafd4d4787c7cf5
Julien Lietard <contact.julienlietard@gmail.com>
2021-04-15 12:52:32 +0200

    modifs

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/my-portfolio'));
+app.use(express.static('./dist/portfolio-julienlietard'));
 
 app.get('/*', (req, res) =>
   res.sendFile('index.html', {root: 'dist/portfolio-julienlietard/'}),
